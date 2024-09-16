d6ed0cbd1bca38f4144c5b4c5e419aee62194ecb
harshitkar <harshitkar104@gmail.com>
2024-09-16 16:02:17 +0530

    Initial commit

---
+++ Solar System/SolarSystem.cpp
@@ -0,0 +1,26 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
+#include <fstream>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+#include <SOIL.h>
+#include <sstream>
+#include <string>
+#include <string>
+#include <vector>
+#include <planet/shader.h>
+#include <planet/camera.h>
+#include <planet/model.h>
+#include <planet/planet.hpp>
+
+#define GLEW_STATIC
+using namespace std;
+
+void main() {
+	int x = planet();
+}
