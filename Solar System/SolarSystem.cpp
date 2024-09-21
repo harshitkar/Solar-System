710d2d03f1f391237dca2917a154931d8f652bae
harshitkar <harshitkar104@gmail.com>
2024-09-21 22:19:16 +0530

    Project Structure Updated

--- Solar System/SolarSystem.cpp
+++ Solar System/SolarSystem.cpp
@@ -18,63 +18,73 @@
 #include "model.h"
 
 #define GLEW_STATIC
+
 using namespace std;
 
+//for planetary revolution
 bool revolve;
 
-const GLint WIDTH = 1400, HEIGHT = 800;
-const double PI = 3.141592653589793238463;
-int SCREEN_WIDTH, SCREEN_HEIGHT;
+//for cursor visibility
+bool cursorVisible;
 
-// Function prototypes
-void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
-void MouseCallback(GLFWwindow* window, double xPos, double yPos);
-void DoMovement();
+// Screen width and height for windowed mode
+bool isFullscreen = false;
+int windowPosX;
+int windowPosY;
+GLint WIDTH = 1400, HEIGHT = 800;
 
 // Camera
-Camera camera(glm::vec3(40.0f, 0.0f, 30.0f));
+Camera camera(glm::vec3(9.0f, 0.0f, 20.0f));
+
+//keycallback variables
 bool keys[1024];
-GLfloat lastX = 400, lastY = 300;
-bool firstMouse = true;
 string cameraType = "";
 
-// Light attributes
+//mousecallback variables
+GLfloat lastX = 400, lastY = 300;
+bool firstMouse = true;
+
+// Light attribute
 glm::vec3 lightPos(0.0f, 0.0f, 0.0f);
+glm::mat4 projection(1);
 
 GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
-
 GLfloat speed = 1.0f;
+const double PI = (22 / 7);
 
+//callback functions
+void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void MouseCallback(GLFWwindow* window, double xPos, double yPos);
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
+
+//other functions
+void ToggleFullscreen(GLFWwindow* window);
+void DoMovement();
 void showMenu();
+
 
 int main() {
 
     showMenu();
     revolve = true;
+    cursorVisible = false;
 
     glfwInit();
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+    glfwWindowHint(GLFW_DECORATED, GL_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "learn Opengl", nullptr, nullptr);
-
-    if (nullptr == window)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-
-        return EXIT_FAILURE;
-    }
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "The solar system", nullptr, nullptr);
 
     glfwMakeContextCurrent(window);
-    glfwGetFramebufferSize(window, &SCREEN_WIDTH, &SCREEN_HEIGHT);
+    glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
     glfwSetKeyCallback(window, KeyCallback);
     glfwSetCursorPosCallback(window, MouseCallback);
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
     glewExperimental = GL_TRUE;
     if (GLEW_OK != glewInit())
     {
@@ -82,7 +92,7 @@ int main() {
         return EXIT_FAILURE;
     }
 
-    glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+    glViewport(0, 0, WIDTH, HEIGHT);
     glEnable(GL_DEPTH_TEST);
 
     Shader* shader = new Shader("resources/shaders/modelLoading.vs", "resources/shaders/modelLoading.frag");
@@ -101,15 +111,22 @@ int main() {
     Model* uranusModel = new Model((GLchar*)"resources/models/uranus/13907_Uranus_v2_l3.obj");
     Model* neptuneModel = new Model((GLchar*)"resources/models/neptune/13908_Neptune_v2_l3.obj");
 
-    glm::mat4 projection(1);
-    projection = glm::perspective(camera.GetZoom(), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
+    projection = glm::perspective(camera.GetZoom(), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
 
-    double radii[8] = {70.0f, 80.0f, 90.0f, 140.0f, };
+    double radii[8] = {70.0f, 80.0f, 90.0f, 110.0f, 140.0f, 180.0f, 210.0f, 240.0f};
 
     GLfloat scale = 0.1f,  angle;
     GLuint i = 0;
     while (!glfwWindowShouldClose(window))
     {
+        if (!cursorVisible) {
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+                    
+        }
+        else {
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        }
+
         GLfloat currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
@@ -119,7 +136,6 @@ int main() {
         DoMovement();
         glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 
         glm::mat4 view(1);
         view = camera.GetViewMatrix();
@@ -166,7 +182,7 @@ int main() {
         model = glm::mat4(1);
         if (revolve) {
             angle = 0.008f * i * speed;
-            radius = 70.0f * scale;
+            radius = radii[0] * scale;
             x = radius * sin(PI * 2 * angle / 360);
             y = radius * cos(PI * 2 * angle / 360);
             model = glm::translate(model, glm::vec3(x, 0.0f, y));
@@ -189,7 +205,7 @@ int main() {
         model = glm::mat4(1);
         if (revolve) {
             angle = 0.007f * i * speed;
-            radius = 80.0f * scale;
+            radius = radii[1] * scale;
             x = radius * sin(PI * 2 * angle / 360);
             y = radius * cos(PI * 2 * angle / 360);
             model = glm::translate(model, glm::vec3(x, 0.0f, y));
@@ -212,7 +228,7 @@ int main() {
         model = glm::mat4(1);
         if (revolve) {
             angle = 0.006f * i * speed;
-            radius = 90.0f * scale;
+            radius = radii[2] * scale;
             x = radius * sin(PI * 2 * angle / 360);
             y = radius * cos(PI * 2 * angle / 360);
             model = glm::translate(model, glm::vec3(x, 0.0f, y));
@@ -235,7 +251,7 @@ int main() {
         model = glm::mat4(1);
         if (revolve) {
             angle = 0.005f * i * speed;
-            radius = 110.0f * scale;
+            radius = radii[3] * scale;
             x = radius * sin(PI * 2 * angle / 360);
             y = radius * cos(PI * 2 * angle / 360);
             model = glm::translate(model, glm::vec3(x, 0.0f, y));
@@ -258,7 +274,7 @@ int main() {
         model = glm::mat4(1);
         if (revolve) {
             angle = 0.0045f * i * speed;
-            radius = 140.0f * scale;
+            radius = radii[4] * scale;
             x = radius * sin(PI * 2 * angle / 360);
             y = radius * cos(PI * 2 * angle / 360);
             model = glm::translate(model, glm::vec3(x, 0.0f, y));
@@ -281,7 +297,7 @@ int main() {
         model = glm::mat4(1);
         if (revolve) {
             angle = 0.0040f * i * speed;
-            radius = 180.0f * scale;
+            radius = radii[5] * scale;
             x = radius * sin(PI * 2 * angle / 360);
             y = radius * cos(PI * 2 * angle / 360);
             model = glm::translate(model, glm::vec3(x, 0.0f, y));
@@ -304,7 +320,7 @@ int main() {
         model = glm::mat4(1);
         if (revolve) {
             angle = 0.0035f * i * speed;
-            radius = 210.0f * scale;
+            radius = radii[6] * scale;
             x = radius * sin(PI * 2 * angle / 360);
             y = radius * cos(PI * 2 * angle / 360);
             model = glm::translate(model, glm::vec3(x, 0.0f, y));
@@ -327,7 +343,7 @@ int main() {
         model = glm::mat4(1);
         if (revolve) {
             angle = 0.003f * i * speed;
-            radius = 240.0f * scale;
+            radius = radii[7] * scale;
             x = radius * sin(PI * 2 * angle / 360);
             y = radius * cos(PI * 2 * angle / 360);
             model = glm::translate(model, glm::vec3(x, 0.0f, y));
@@ -362,7 +378,7 @@ int main() {
         sunModel->Draw(*lampShader);
 
         if (cameraType == "Up") {
-            camera.SetPosition(glm::vec3(-1.438195, 38.160343, 1.159209));
+            camera.SetPosition(glm::vec3(0, 40, 0));
         }
 
         glfwSwapBuffers(window);
@@ -383,24 +399,30 @@ void showMenu() {
     std::cout << "Control Simulation Speed:\n";
     std::cout << "  Increase or decrease the simulation speed.\n";
     std::cout << "  Actions:\n";
-    std::cout << "    Increase speed: Press + \n";
-    std::cout << "    Decrease speed: Press - \n\n";
+    std::cout << "    Increase speed: Press +. \n";
+    std::cout << "    Decrease speed: Press -. \n\n";
 
     std::cout << "Change Camera View:\n";
     std::cout << "  Switch between different planet cameras for an immersive experience.\n";
     std::cout << "  Actions:\n";
     std::cout << "    View Mercury: Press 1.\n";
-    std::cout << "    View Venus: Press 2.\n";
-    std::cout << "    View Earth: Press 3.\n";
-    std::cout << "    View Mars: Press 4.\n";
+    std::cout << "    View Venus:   Press 2.\n";
+    std::cout << "    View Earth:   Press 3.\n";
+    std::cout << "    View Mars:    Press 4.\n";
     std::cout << "    View Jupiter: Press 5.\n";
-    std::cout << "    View Saturn: Press 6.\n";
-    std::cout << "    View Uranus: Press 7.\n";
+    std::cout << "    View Saturn:  Press 6.\n";
+    std::cout << "    View Uranus:  Press 7.\n";
     std::cout << "    View Neptune: Press 8.\n";
-    std::cout << "    Free Camera: Press F.\n\n";
+    std::cout << "    Free Camera:  Press 0.\n\n";
 
     std::cout << "Pause and Resume Revolution Simulation:\n";
     std::cout << "  Actions: Press P.\n\n";
+
+    std::cout << "Toggle cursor visibility\n";
+    std::cout << "  Actions: Press X.\n";
+
+    std::cout << "Toggle Fullscreen:\n";
+    std::cout << "  Action: Press F11.\n\n";
 
     std::cout << "Exit the Simulation:\n";
     std::cout << "  Action: Press ESC.\n\n\n";
@@ -471,20 +493,43 @@ void DoMovement() {
             speed = 0;
         }
     }
-    if (keys[GLFW_KEY_M]) {
-        if (speed <= 10)
+    else if (keys[GLFW_KEY_M]) {
+        if (speed <= 100)
         speed = speed + 0.1f;
     }
     else if (keys[GLFW_KEY_N]) {
         if (speed >= 0.1f)
         speed = speed - 0.1f;
     }
+    else if (keys[GLFW_KEY_X]) {
+        cursorVisible = !cursorVisible;
+    }
+}
+
+void ToggleFullscreen(GLFWwindow* window) {
+    if (isFullscreen) {
+        glfwSetWindowMonitor(window, nullptr, windowPosX, windowPosY, WIDTH, HEIGHT, 0);
+    }
+    else {
+        GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+        const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+
+        glfwGetWindowPos(window, &windowPosX, &windowPosY);
+        glfwGetWindowSize(window, &WIDTH, &HEIGHT);
+
+        glfwSetWindowMonitor(window, monitor, 0, 0, mode->width, mode->height, mode->refreshRate);
+    }
+    isFullscreen = !isFullscreen;
 }
 
 
 void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode) {
     if (GLFW_KEY_ESCAPE == key && GLFW_PRESS == action) {
         glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+
+    if (key == GLFW_KEY_F11 && action == GLFW_PRESS) {
+        ToggleFullscreen(window);
     }
 
     if (key >= 0 && key < 1024) {
@@ -511,4 +556,14 @@ void MouseCallback(GLFWwindow* window, double xPos, double yPos) {
     lastY = yPos;
 
     camera.ProcessMouseMovement(xOffset, yOffset);
+}
+
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height) {
+    WIDTH = width;
+    HEIGHT = height;
+
+    glViewport(0, 0, WIDTH, HEIGHT);
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    projection = glm::perspective(camera.GetZoom(), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
 }
